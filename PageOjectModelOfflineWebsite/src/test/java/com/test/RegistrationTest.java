773cdb4fac0ca905c1f5fc298f06f8c2956c8d63
ashwinid111 <ashwinid111@gmail.com>
2020-08-04 09:48:51 +0530

    new code

--- PageOjectModelOfflineWebsite/src/test/java/com/test/RegistrationTest.java
+++ PageOjectModelOfflineWebsite/src/test/java/com/test/RegistrationTest.java
@@ -1,65 +1,87 @@
 package com.test;
 
-import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import com.pages.DownloadPage;
 import com.pages.RegistrationPage;
 
 public class RegistrationTest extends TestBase {
-	
+
 	RegistrationPage register = null;
 
-	
+	// Extent Report Variables
+	String pageName = "REGISTRATION PAGE TESTCASES";
+	String extentReprtName = "RegistrationPageExtentReport";
+	String hostName = "Offline Website";
+	String environment = "Registration Page Testing";
+	String uName = "ketaki chalse";
+	String docTitle = "RegistrationPage";
+	String reportName = "RegistrationPageExtentReport ";
+
+	// ExtentReport Methods
+	@BeforeTest()
+	public void beforeTest() {
+		super.setReport(extentReprtName, hostName, environment, uName, docTitle, reportName);
+	}
+
+	@AfterTest
+	public void afterTest() {
+		super.endReport();
+	}
+
 	@BeforeMethod
 	public void loadUrl() throws Throwable {
-		driver=super.launchApplication();
+		driver = super.launchApplication();
 		register = new RegistrationPage(driver);
 	}
-	
 
-	
 	@Test(priority = 1)
 	public void checkRegistrationLink() {
-		Assert.assertTrue(register.validateRegistraionLink(driver));
+		super.passTest("check Registration Link", pageName);
+		Assert.assertTrue(register.validateRegistraionLink(testlogger));
 	}
 
 	@Test(priority = 2)
 	public void checkHeading() {
-		Assert.assertTrue(register.getTextOfHeading(driver));
+		super.passTest("check heading of Registration page", pageName);
+		Assert.assertTrue(register.getTextOfHeading(testlogger));
 	}
 
 	@Test(priority = 3)
 	public void checkTitle() {
-		Assert.assertTrue(register.getTitleOfRegistrationPage(driver));
+		super.passTest("check Title of Registration page", pageName);
+		Assert.assertTrue(register.getTitleOfRegistrationPage(testlogger));
 	}
 
 	@Test(priority = 4)
 	public void checkTextBoxCounting() {
-		Assert.assertTrue(register.validateNoOfTextBoxes(driver));
+		super.passTest("check Total no of Tex Box of Registration page", pageName);
+		Assert.assertTrue(register.validateNoOfTextBoxes(testlogger));
 	}
 
 	@Test(priority = 5)
 	public void checkIfUserAdded() throws Throwable {
+		super.passTest("Enter the new member details and accept alert", pageName);
 		register.EnterName();
 		register.EnterMobile();
 		register.EnterEmail();
 		register.EnterPassword();
-		Assert.assertTrue(register.validateAlertMessage(driver));
+		Assert.assertTrue(register.validateAlertMessage(testlogger));
 	}
 
 	@Test(priority = 6)
 	public void checkIfMembershipExist() {
-		Assert.assertTrue(register.validationOfMembership(driver));
+		super.passTest("check if the member is already exist", pageName);
+		Assert.assertTrue(register.validationOfMembership(testlogger));
 	}
-	
+
 	@AfterMethod
-	public void closeApplication(){
+	public void closeApplication() {
+		super.testLogger().info("Browser Closed");
 		super.CloseLaunchApplication();
 	}
 
