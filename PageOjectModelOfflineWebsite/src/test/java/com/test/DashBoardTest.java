1a20bd7f246afe067c489da5ec8dad871e93e1f4
utkarsh <utkarshpande261@gmail.com>
2020-08-04 20:18:52 +0530

    utkarsh added testcaselogs method in testbaseclass

--- PageOjectModelOfflineWebsite/src/test/java/com/test/DashBoardTest.java
+++ PageOjectModelOfflineWebsite/src/test/java/com/test/DashBoardTest.java
@@ -1,12 +1,11 @@
 package com.test;
 
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentTest;
 import com.pages.DashBoardPage;
 import com.pages.LoginPage;
 
@@ -14,6 +13,12 @@ public class DashBoardTest extends TestBase {
 	DashBoardPage dp;
 	LoginPage lp;
 
+	ExtentTest extenttestPerPage = null;
+
+	@BeforeClass()
+	public void beforeclass() {
+		extenttestPerPage = testlogger.createNode("DashBoard TestCases");
+	}
 	@BeforeMethod
 	public void loadUrl() throws Throwable {
 		driver=super.launchApplication();
@@ -21,30 +26,56 @@ public class DashBoardTest extends TestBase {
 
 		dp=lp.NavigateToDashboard();
 	}
-	@AfterMethod
-	public void closeBrowser() {
-	//	testLoggerLog4j().info( "Browser Closed");
-		super.CloseLaunchApplication();
-	}
-	// Extent Report Variables
-	String pageName = "DASHBOARD PAGE TESTCASES";
-	String extentReprtName = "DashboardPageExtentReport";
-	String hostName = "Offline Website";
-	String environment = "QA";
-	String uName = "utkarsh";
-	String docTitle = "DashboardPage";
-	String reportName = "DashboardpageExtentReport ";
-
-	// ExtentReport Methods..
-	@BeforeTest()
-	public void beforeTest() {
-	//	super.setReport(extentReprtName, hostName, environment, uName, docTitle, reportName);
+	
+	@Test(priority = 1)
+	public void checkTitle() {
+		ExtentTest extentTest=extenttestPerPage.createNode("test case  :: checkTitle");
+		super.testcaseLogs().info("Running checkTitle");
+		Assert.assertTrue(dp.verifyTitleOfPage(extentTest));
 	}
 
-	@AfterTest
-	public void afterTest() {
-		super.endReport();
+	@Test(priority = 2)
+	public void checkLogoutLabel() {
+		ExtentTest extentTest=extenttestPerPage.createNode("test case  :: checkLogoutLabel");
+		super.testcaseLogs().info("Running checkLogoutLabel");
+		Assert.assertTrue(dp.verifyLogoutLabel(extentTest));
 	}
+
+	@Test(priority = 3)
+	public void checkPageHeaderText() {
+		ExtentTest extentTest=extenttestPerPage.createNode("test case  :: checkPageHeaderText");
+		super.testcaseLogs().info("Running checkPageHeaderText");
+		Assert.assertTrue(dp.verifyHeader(extentTest));
+	}
+
+	@Test(priority = 4)
+	public void checkAllCoursesHeaderText() {
+		ExtentTest extentTest=extenttestPerPage.createNode("test case  :: checkAllCoursesHeaderText");
+		super.testcaseLogs().info("Running checkAllCoursesHeaderText");
+		Assert.assertTrue(dp.verifyCourseHeaders(extentTest));
+	}
+
+	@Test(priority = 5)
+	public void checkNumberOfCourses() {
+		ExtentTest extentTest=extenttestPerPage.createNode("test case  :: checkNumberOfCourses");
+		super.testcaseLogs().info("Running checkNumberOfCourses");
+		Assert.assertTrue(dp.verifyNumberOfCourses(extentTest));
+	}
+
+	@Test(priority = 6)
+	public void checkAllCourseslinksClickable() {
+		ExtentTest extentTest=extenttestPerPage.createNode("test case  :: checkAllCourseslinksClickable");
+		super.testcaseLogs().info("Running checkAllCourseslinksClickable");
+		Assert.assertTrue(dp.verifyAllCourseslinksClickable(extentTest));
+	}
+
+	@Test(priority = 7)
+	public void checkAllCoursesNames() {
+		ExtentTest extentTest=extenttestPerPage.createNode("test case  :: checkAllCoursesNames");
+		super.testcaseLogs().info("Running checkAllCoursesNames");
+		Assert.assertTrue(dp.verifyCourseNames(extentTest));
+	}
+	
 
 	
 }
