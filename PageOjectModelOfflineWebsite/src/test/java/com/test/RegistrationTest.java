e8f64c45a9ecde330e1f2a37f0847d7dacb50926
ketakichalse19 <ketakichalse24@gmail.com>
2020-08-05 09:03:09 +0000

    Extent report update in Registration Test

--- PageOjectModelOfflineWebsite/src/test/java/com/test/RegistrationTest.java
+++ PageOjectModelOfflineWebsite/src/test/java/com/test/RegistrationTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 import com.pages.RegistrationPage;
 
 public class RegistrationTest extends TestBase {
@@ -27,7 +28,8 @@ public class RegistrationTest extends TestBase {
 	@Test(priority = 1)
 	public void checkRegistrationLink() {
 		ExtentTest extentTest = extenttestPerPage.createNode("test case  :: checkRegistrationLink");
-		super.testcaseLogs().info("checkRegistrationLink");
+		extentTest.log(Status.INFO, "I am in Registration page test...checkRegistrationLink");
+		logger.info("checkRegistrationLink");
 		Assert.assertTrue(register.validateRegistraionLink(extentTest));
 
 	}
@@ -35,28 +37,32 @@ public class RegistrationTest extends TestBase {
 	@Test(priority = 2)
 	public void checkHeading() {
 		ExtentTest extentTest = extenttestPerPage.createNode("test case  :: checkHeading");
-		super.testcaseLogs().info("checkHeading");
+		extentTest.log(Status.INFO, "I am in Registration page test...checkHeading");
+		logger.info("checkHeading");
 		Assert.assertTrue(register.getTextOfHeading(extentTest));
 	}
 
-	@Test(priority = 3)
+	/*@Test(priority = 3)
 	public void checkTitle() {
 		ExtentTest extentTest = extenttestPerPage.createNode("test case  :: checkTitle");
-		super.testcaseLogs().info("checkTitle");
+		extentTest.log(Status.INFO, "I am in Registration page test...checkTitle");
+		logger.info("checkTitle");
 		Assert.assertTrue(register.getTitleOfRegistrationPage(extentTest));
 	}
 
 	@Test(priority = 4)
 	public void checkTextBoxCounting() {
 		ExtentTest extentTest = extenttestPerPage.createNode("test case  :: checkTextBoxCounting");
-		super.testcaseLogs().info("checkTextBoxCounting");
+		extentTest.log(Status.INFO, "I am in Registration page test...checkTextBoxCounting");
+		logger.info("checkTextBoxCounting");
 		Assert.assertTrue(register.validateNoOfTextBoxes(extentTest));
 	}
 
 	@Test(priority = 5)
 	public void checkIfUserAdded() throws Throwable {
 		ExtentTest extentTest = extenttestPerPage.createNode("test case  :: checkIfUserAdded");
-		super.testcaseLogs().info("checkIfUserAdded");
+		extentTest.log(Status.INFO, "I am in Registration page test...checkIfUserAdded");
+		logger.info("checkIfUserAdded");
 		register.EnterName();
 		register.EnterMobile();
 		register.EnterEmail();
@@ -67,8 +73,9 @@ public class RegistrationTest extends TestBase {
 	@Test(priority = 6)
 	public void checkIfMembershipExist() {
 		ExtentTest extentTest = extenttestPerPage.createNode("test case  :: checkIfMembershipExist");
-		super.testcaseLogs().info("checkIfMembershipExist");
+		extentTest.log(Status.INFO, "I am in Registration page test...checkIfMembershipExist");
+		logger.info("checkIfMembershipExist");
 		Assert.assertTrue(register.validationOfMembership(extentTest));
 	}
-
+*/
 }
